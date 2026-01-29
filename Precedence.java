a9262ed1da781c0eaf484d89f3b5fea6695a4a86
dharunprakashgt14 <dharunprakashgt14@gmail.com>
2026-01-29 10:05:12 +0530

    6th commit by me

---
+++ Precedence.java
@@ -0,0 +1,7 @@
+public class Precedence {
+    public static void main(String[] args) {
+        int a=3,b=2,c=1;
+        int result=++a*b--+c<<2/2+1;
+        System.out.println(result);
+    }
+}
