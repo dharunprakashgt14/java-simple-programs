65134848eb822d6c79676ce18d631d87fafd2d1c
dharunprakashgt14 <dharunprakashgt14@gmail.com>
2026-01-29 09:44:56 +0530

    4th commit by me

---
+++ JavaSwitch.java
@@ -0,0 +1,19 @@
+public class JavaSwitch{
+    public static void main(String[]args) {
+        String day ="Wednesday";
+        switch (day){
+            case "Sunday":
+                System.out.println("Today is Sunday");
+                break;
+                case "Monday":
+                System.out.println("Today is Monday");
+                 break;
+                case "Tuesday":
+                System.out.println("Today is Tuesday");
+                 break;
+                case "Wednesday":
+                    System.out.println("Today is Wednesday");
+                break;
+        }
+    }
+}
