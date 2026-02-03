2687f847634efbc6e21e0c09b72d800f15fc95ab
dharunprakashgt14 <dharunprakashgt14@gmail.com>
2026-02-03 22:02:02 +0530

    23rd commit by me

---
+++ LargestOfTwo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LargestOfTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > b) {
+            System.out.println(a + " is larger");
+        } else {
+            System.out.println(b + " is larger");
+        }
+    }
+}
