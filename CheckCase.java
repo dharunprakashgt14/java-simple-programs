063b571913391501f4169305736b8894938a9fbc
dharunprakashgt14 <dharunprakashgt14@gmail.com>
2026-02-12 22:10:12 +0530

    40th commit by me

---
+++ CheckCase.java
@@ -0,0 +1,13 @@
+public class CheckCase {
+    public static void main(String[] args) {
+        char ch = 'A';
+
+        if (ch >= 65 && ch <= 90) {
+            System.out.println("Uppercase");
+        } else if (ch >= 97 && ch <= 122) {
+            System.out.println("Lowercase");
+        } else {
+            System.out.println("Not an alphabet");
+        }
+    }
+}
