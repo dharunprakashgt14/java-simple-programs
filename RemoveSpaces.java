a4cf57227dcdb33d200b433beee6ec90f0879012
dharunprakashgt14 <dharunprakashgt14@gmail.com>
2026-02-15 03:21:03 +0530

    RemoveSpaces

---
+++ RemoveSpaces.java
@@ -0,0 +1,8 @@
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        String str = "I am learning Java";
+
+        String result = str.replace(" ", "");
+        System.out.println(result);
+    }
+}
