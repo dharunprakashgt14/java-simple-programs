7e4b8d0d74314dd3d636694512bf100e65009e75
dharunprakashgt14 <dharunprakashgt14@gmail.com>
2026-02-20 23:53:31 +0530

    CommonElements

---
+++ CommonElements.java
@@ -0,0 +1,16 @@
+public class CommonElements {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4};
+        int[] b = {3, 4, 5, 6};
+
+        System.out.print("Common elements: ");
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                if (a[i] == b[j]) {
+                    System.out.print(a[i] + " ");
+                }
+            }
+        }
+    }
+}
